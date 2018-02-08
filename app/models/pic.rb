969827d1fc861a171eae249ff764751d64dfd566
Ella Queen <ella@diamondsis.com>
2018-02-07 22:13:39 -0800

    New photos

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -2,6 +2,6 @@ class Pic < ApplicationRecord
 	acts_as_votable
 	belongs_to :user
 
-	has_attached_file :image, styles: { medium: "300x300>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :image, styles: { large: "600x600>", medium: "300x300>" }, default_url: "/images/:style/missing.png"
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
